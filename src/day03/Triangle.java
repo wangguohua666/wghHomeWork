ef9b13d75e4064752a5521ca657e6489416b5ed7
wangguohua666 <2459343633@qq.com>
2020-06-17 15:01:12 +0800

    王国华第一次作业

--- src/day03/Triangle.java
+++ src/day03/Triangle.java
@@ -2,6 +2,15 @@ package day03;
 
 public class Triangle {
     public static void main(String[] args) {
-        for
+        int i,j,k;
+        for(i=0;i<6;i++){
+            for(k=0;k<6-i;k++){
+                System.out.print(" ");
+            }
+            for(j=10-i;j<=10+i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ src/day03/chengfabiao99.java
@@ -0,0 +1,13 @@
+package day03;
+
+public class chengfabiao99 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<10;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(j+"*"+i+"="+i*j+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
